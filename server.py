6618f10e1a7d5acbc72d1d6426be5d15c1a8958b
DavydovMI <mapkgold@gmail.com>
2021-11-16 12:16:03 +0300

    Create /api/test server

--- server.py
+++ server.py
@@ -11,6 +11,18 @@ app.debug = False
 @app.route('/api/test', methods=['POST'])
 def test():
     r = request
+
+    nparr = np.fromstring(r.data, np.uint8)
+    filename = 'base_img.jpg'
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    counter = save_original_image(img, filename)
+    extension = filename.find('.')
+    add_to_origin_table(filename)
+    path_to_original = os.path.abspath(os.curdir) + '\\Original\\' + filename[:extension] + str(counter) + filename[extension:]
+    edited_name = 'edited.jpg'
+
+    path_to_watermark = "w.png"
+
     return Response(img_encoded.tostring())
 
 
